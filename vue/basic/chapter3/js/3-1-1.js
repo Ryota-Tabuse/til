bd01ebff7fc5f983beb85421c9e7b9a9aa7c72eb
Ryota-Tabuse <bububutase.0107@gmail.com>
2020-02-26 01:00:14 +0900

    イベントのきほん

---
+++ vue/basic/chapter3/js/3-1-1.js
@@ -0,0 +1,34 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        message:'',
+        result:'',
+        path:'http://www.web-deli.com/image/linkbanner_l.gif'
+    },
+    methods: {
+        onclick: function() {
+            this.message = new Date().toLocaleString();
+        },
+        onmouseenter: function() {
+            this.path = 'http://www.web-deli.com/image/home_chara.gif';
+        },
+        onmouseleave: function() {
+            this.path = 'http://www.web-deli.com/image/linkbanner_l.gif';
+        },
+        onmousein: function(e) {
+            this.result += 'Enter:' + e.target.id + '<br />';
+        },
+        onmouseout: function(e) {
+            this.result += 'Leave:' + e.target.id + '<br />';
+        }
+    }
+});
+let app2 = new Vue({
+    el: '#app2',
+    data: {
+        result:'',
+    },
+    methods: {
+        
+    }
+});
