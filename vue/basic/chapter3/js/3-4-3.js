6c1a138e1efb702a359eb9ec9932a399137956dc
Ryota-Tabuse <bububutase.0107@gmail.com>
2020-02-29 22:41:28 +0900

    データバインディング関連のディレクティブ

---
+++ vue/basic/chapter3/js/3-4-3.js
@@ -0,0 +1,17 @@
+new Vue({
+    el: '#app',
+    data: {
+        name: '匿名',
+        color: {
+            backgroundColor :'Green',
+            color: 'white'
+        },
+        size: {
+            fontSize: '1.5rem'
+        },
+        url:'http://wings.msn.to/',
+        color2: true,
+        isChange: true,
+        message: '皆さんこんにちは'
+    }
+});
