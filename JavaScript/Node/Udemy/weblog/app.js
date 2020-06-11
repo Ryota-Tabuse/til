09e3715a7a9a56d8f4f829bea5353cb26b967cb1
Ryota-Tabuse <bububutase.0107@gmail.com>
2020-06-11 22:54:02 +0900

    AccessLog

--- JavaScript/Node/Udemy/weblog/app.js
+++ JavaScript/Node/Udemy/weblog/app.js
@@ -1,1 +1,19 @@
-console.log(process.env.NODE_ENV);
+const accesslogger = require('./lib/log/accesslogger.js');
+const systemlogger = require('./lib/log/systemlogger.js');
+const express = require('express');
+const app = express();
+
+// ejsの使用
+app.set('view engine', 'ejs');
+app.disable('x-powered-by');
+
+// 静的ファイルの配信
+app.use('/public', express.static(__dirname + '/public/' + (process.env.NODE_ENV === 'development' ? 'development' : 'production')));
+
+app.use(accesslogger());
+
+app.use('/', require('./routes/index.js'));
+
+app.use(systemlogger());
+
+app.listen(3000);
