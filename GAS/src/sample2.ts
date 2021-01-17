5536fa9b735c7ecd7e9f1de2b5258495b5a1d1c0
Ryota-Tabuse <ryota.tabuse@gmail.com>
2021-01-18 00:12:19 +0900

    add comment

--- GAS/src/sample2.ts
+++ GAS/src/sample2.ts
@@ -1,11 +1,14 @@
 function myFunction() {
+    // シートが一つの場合
     const sheet: any = SpreadsheetApp.getActiveSheet();
     Logger.log(sheet.getName());
 
+    //シート名を直接指定
     const spreadsheet: any = SpreadsheetApp.getActiveSpreadsheet();
     const sheetByName: any = spreadsheet.getSheetByName("シート2");
     Logger.log(sheetByName.getName());
 
+    // 配列から取得
     const spreadsheet2: any = SpreadsheetApp.getActiveSpreadsheet();
     const sheets: any[] = spreadsheet2.getSheets();
     Logger.log(sheets[0].getName());
