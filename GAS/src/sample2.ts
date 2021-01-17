5b9279568d6a2ac8d90ac495905a51b7042d6487
Ryota-Tabuse <ryota-tabuse@gmail.com>
2021-01-18 00:02:49 +0900

    getByName

--- GAS/src/sample2.ts
+++ GAS/src/sample2.ts
@@ -2,4 +2,10 @@ function myFunction() {
     // 紐づくスプレッドシートの取得方法
     const sheet: any = SpreadsheetApp.getActiveSheet();
     Logger.log(sheet.getName());
+
+    // 紐づくスプレッドシートの取得方法
+    const spreadsheet: any = SpreadsheetApp.getActiveSpreadsheet();
+    const sheetByName: any = spreadsheet.getSheetByName("シート2");
+    Logger.log(sheetByName.getName());
+
 }
