c6be3bc6b430369176a8ad807b2e8e7738fa539b
Ryota-Tabuse <ryota.tabuse@gmail.com>
2022-02-03 23:43:49 +0900

    Python Study

---
+++ Competitive programming/Python_test/test.py
@@ -0,0 +1,14 @@
+
+# algo式
+
+# 標準入力 1-3
+# N = input()
+# print(N * 3)
+
+# 標準入力 1-4
+# S = input()
+# print(S[2])
+
+# 標準入力 1-5
+S = int(input())
+print(24-S)
---
+++ NextJS/nextjs_hp/pages/blog-page.js
@@ -0,0 +1,8 @@
+import Layout from "../components/Layout";
+import Image from "next/image";
+
+function Blog() {
+  return <Layout title="Blog"></Layout>;
+}
+
+export default Blog;
