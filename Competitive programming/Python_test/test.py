9621be32fc37939b21a658fa4fd7e7f3d646e8d7
Ryota-Tabuse <ryota.tabuse@gmail.com>
2022-02-06 17:41:10 +0900

    algo式 ちょっと勉強

--- Competitive programming/Python_test/test.py
+++ Competitive programming/Python_test/test.py
@@ -10,5 +10,25 @@
 # print(S[2])
 
 # 標準入力 1-5
-S = int(input())
-print(24-S)
+# S = int(input())
+# print(24-S)
+
+# 標準入力 2-1
+# A ,B = map(int, input().split())
+# print(A + B)
+
+# 標準入力 2-2
+# A ,B = map(int, input().split())
+# if A>B:
+#     print(A)
+# else :
+#     print(B)
+
+# 標準入力 2-3
+# A, B = map(int, input().split())
+# if A%10<B%10:
+#     print(A)
+# else :
+#     print(B)
+
+
